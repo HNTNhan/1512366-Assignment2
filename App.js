ffa1941592274ebedae4de2dda3b2198c7b7972e
Nhan <1512366@student.hcmus.edu.vn>
2020-05-07 13:23:29 +0700

    create button, square

--- App.js
+++ App.js
@@ -1,10 +1,71 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
 
 export default function App() {
+  const [color, setColor] = useState('white');
+  let [history, setHistory] = useState(['white']);
+  let [pos, setPos] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.containerButton}>
+        <Button
+          buttonStyle={[styles.buttonColor, {backgroundColor: 'red'}]}
+          onPress={() => {
+            if(pos<history.length-1) history.splice(pos+1);
+            history.push('red');
+            setHistory(history);
+            setPos(pos+=1)
+            setColor('red');
+          }}
+        />
+        <Button
+          buttonStyle={[styles.buttonColor, {backgroundColor: 'green'}]}
+          onPress={() => {
+            if(pos<history.length-1) history.splice(pos+1);
+            history.push('green');
+            setHistory(history);
+            setPos(pos+=1);
+            setColor('green');
+          }}
+        />
+        <Button
+          buttonStyle={[styles.buttonColor, {backgroundColor: 'blue'}]}
+          onPress={() => {
+            if(pos<history.length-1) history.splice(pos+1);
+            history.push('blue')
+            setHistory(history);
+            setPos(pos+=1)
+            setColor('blue');
+          }}
+        />
+        <Button
+          buttonStyle={[styles.buttonColor, styles.buttonAction]}
+          titleStyle={styles.buttonTitle}
+          title= 'Undo'
+          onPress={() => {
+            pos > 0 ? pos -= 1: pos;
+            setPos(pos)
+            setColor(history[pos]);
+          }}
+        />
+        <Button
+          buttonStyle={[styles.buttonColor, styles.buttonAction]}
+          titleStyle={styles.buttonTitle}
+          title='Redo'
+          onPress={() => {
+            pos < history.length-1 ? pos += 1: pos;
+            setPos(pos)
+            setColor(history[pos]);
+          }}
+        />
+      </View>
+      <View style={styles.containerSquare}>
+        <View style={[styles.square, {backgroundColor: color}]}>
+        </View>
+      </View>
+
     </View>
   );
 }
@@ -12,8 +73,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  containerButton: {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    marginTop: 40,
+    marginLeft: 10,
+  },
+  buttonColor: {
+    width: 40,
+    height: 40,
+    marginRight: 10,
+  },
+  buttonAction: {
+    padding: 0,
+    backgroundColor: 'white',
+    borderWidth: 2,
+    borderColor: 'black',
+  },
+  buttonTitle: {
+    fontSize: 13,
+    color: 'black',
+  },
+  containerSquare: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    flexDirection: 'column',
+  },
+  square: {
+    width: 120,
+    height: 120,
+    borderWidth: 2,
+  }
 });
